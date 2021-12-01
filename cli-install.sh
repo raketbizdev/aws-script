8d9adaf7f5b0442aa1fc627f87ecb560c83c54e6
Admin <admin@Admins-MacBook-Pro.local>
2021-12-01 22:46:13 +0800

    add config

--- cli-install.sh
+++ cli-install.sh
@@ -2,8 +2,12 @@
 # Author name: Ruel Nopal
 # Company: Ruel Nopal - IT Consultant.
 
+# sudo wget -O https://raw.githubusercontent.com/raketbizdev/aws-script/main/cli-install.sh; sudo chmod 755 cli-install.sh; sudo rm -f cli-install.sh;
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 
 aws --version
+
+aws configure
