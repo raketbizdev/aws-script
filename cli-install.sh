31243848f0d0b4daa8c1e589761deef5e9d31fd2
Admin <admin@Admins-MacBook-Pro.local>
2021-12-01 23:00:36 +0800

    sjsg

--- cli-install.sh
+++ cli-install.sh
@@ -2,7 +2,7 @@
 # Author name: Ruel Nopal
 # Company: Ruel Nopal - IT Consultant.
 
-# sudo wget -O https://raw.githubusercontent.com/raketbizdev/aws-script/main/cli-install.sh; sudo chmod 755 cli-install.sh; sudo rm -f cli-install.sh;
+# sudo wget https://raw.githubusercontent.com/raketbizdev/aws-script/main/cli-install.sh; sudo chmod 755 cli-install.sh; sudo rm -f cli-install.sh;
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
